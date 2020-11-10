4cef4e291ca7fe8049a1ccf2ef5bf053d78ef5eb
Vitaliy Kramar <kramarenko.199@gmail.com>
2020-11-10 17:47:18 +0200

    Begin develop mobile ver

---
+++ js/hamburger.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $('.menu-button').click(function(event) {
+       $('.hamburger-menu,.header-menu').toggleClass('active');
+      //  $('body').toggleClass('lock');
+  });
+});
